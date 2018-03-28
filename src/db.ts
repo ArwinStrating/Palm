aea00310e8dd4a2e1e7fc8402d09c94c640bf843
Arwin Strating <arwin.strating@gmail.com>
2018-03-28 12:04:11 +0200

    Resolved firebase firestore import issue

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,12 @@
 import * as firebase from "firebase";
+import 'firebase/firestore'
 
 const config = {
     apiKey: "AIzaSyDz3ZjOqctGGTCz-sZj_eq924vSMen2QN4",
     authDomain: "m4m-code-heroes-dev.firebaseapp.com",
     databaseURL: "https://m4m-code-heroes-dev.firebaseio.com",
     storageBucket: "m4m-code-heroes-dev.appspot.com",
+    projectId: "m4m-code-heroes-dev"
 };
-const admin = firebase.initializeApp(config);
-export const db = admin.firestore();
+firebase.initializeApp(config);
+export const db = firebase.firestore();
